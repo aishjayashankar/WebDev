2a3f80d79a884b22d37f486bce9c22d93ffdd025
Aishwarya Jayashankar <37510042+aishjayashankar@users.noreply.github.com>
2019-07-29 15:54:34 +0530

    Add files via upload

--- myjs1.js
+++ myjs1.js
@@ -2,20 +2,24 @@ num1 = 20;
 var num2 = 30;
 result = num1 + num2;
 document.write("The result is \n"+result);
+document.write("<br>");
 //single line
 /*multi
 line*/
+
             
 var team1 = 99;
 var team2 = 98;
 if(team1>team2)
 {
-document.write(" team one is highest");
+document.write(" team 1 is highest");
 }
 else
 {
 document.write("team 2 is highest");
 }
+document.write("<br>");
+
 
 function add(one,two)
 {
@@ -24,6 +28,8 @@ function add(one,two)
 }
 
 document.write(" Sum from function is "+ add(10,20));
+document.write("<br>");
+
 
 var amt=99;//float
 var city="bangalore";//string
@@ -31,6 +37,7 @@ var state='karnataka';//string
 var var1=true;//bool
 
 // + - * / % = == > >= < <= && || !
+
 
 var d=new Date();
 //alert(d);
@@ -49,6 +56,8 @@ else
     {
         document.write("good evening");
     }
+document.write("<br>");
+
 
 //switch; more conditions, faster
 
@@ -68,4 +77,39 @@ switch (day)
         default:
             document.write("Define your day");
     }
+document.write("<br>");
+
+
+/*iteration
+while*/
+
+var i=0;
+while(i<=5)
+    {
+        document.write(i+" ");
+        i++;
+    }
+document.write("<br>");
+
+var i=0;
+while(i<=10)
+    {
+        document.write(i+" ");
+        i=i+2;
+    }
+document.write("<br>");
+
+var i=11;
+while(i>=3)
+    {
+        document.write(i+" ");
+        i=i-2;
+    }
+document.write("<br>");
+
+for(var i=0;i<10;i++)
+    {
+        document.write(i+" ");
+    }
+
 
